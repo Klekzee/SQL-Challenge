04cc7b637b45e83dae12875053206c4dd8935392
Klenn <johnklenn@gmail.com>
2024-04-24 14:52:46 +0800

    Normalized pizza_recipes

--- 02-PizzaRunner/02query.sql
+++ 02-PizzaRunner/02query.sql
@@ -385,3 +385,47 @@ SELECT
 FROM CTE_successful_deliveries AS sd
 JOIN CTE_total_deliveries AS td
     ON td.runner_id = sd.runner_id;
+
+
+
+
+
+----------------------------------------------------------------------------------------------------------
+
+-- C. Ingredient Optimisation
+
+----------------------------------------------------------------------------------------------------------
+-- Normalizing the pizza_recipes table
+
+SELECT * FROM pizza_recipes;
+
+DROP TABLE IF EXISTS pizza_recipes_normalized;
+CREATE TABLE pizza_recipes_normalized (
+    pizza_id INT,
+    toppings INT
+);
+
+INSERT INTO pizza_recipes_normalized (pizza_id, toppings)
+VALUES
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (1, 5),
+    (1, 6),
+    (1, 8),
+    (1, 10),
+    (2, 4),
+    (2, 6),
+    (2, 7),
+    (2, 9),
+    (2, 11),
+    (2, 12);
+
+SELECT * FROM pizza_recipes_normalized;
+
+
+
+----------------------------------------------------------------------------------------------------------
+-- 1. What are the standard ingredients for each pizza?
+
